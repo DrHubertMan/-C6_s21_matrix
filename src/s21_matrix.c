0e2d3cc9cb958a6977a9bd7f60f83eee06ce12df
Mammie Mikken <mammiemi@at-k5.msk.21-school.ru>
2023-01-25 20:24:49 +0300

    feat:mult_number/matrix

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -1,10 +1,18 @@
 #include "s21_matrix.h"
 
 int main() {
-  matrix_t test;
-  s21_create_matrix(4, 3, &test);
-  s21_print_matrix(test);
-  s21_remove_matrix(&test);
+  matrix_t test_one, test_two, test_result;
+  s21_create_matrix(1, 2, &test_one);
+  s21_create_matrix(2, 5, &test_two);
+  s21_random_matrix(&test_one);
+  s21_random_matrix(&test_two);
+  s21_mult_matrix(&test_one, &test_two, &test_result);
+  s21_print_matrix(test_one);
+  s21_print_matrix(test_two);
+  s21_print_matrix(test_result);
+  s21_remove_matrix(&test_result);
+  s21_remove_matrix(&test_one);
+  s21_remove_matrix(&test_two);
   return 0; 
 }
 
@@ -22,7 +30,7 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
     int result = 1;
     if (s21_valid(A) && s21_valid(B) && s21_compare_matrix(A, B)) {
         for (int i = 0; i < A->rows; i++) {
-            for (int j = 0; A->columns; j++) {
+            for (int j = 0; j < A->columns; j++) {
                 if(A->matrix[i][j] - B->matrix[i][j] > EPS)
                 result = 0;
             }
@@ -67,13 +75,67 @@ int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
   return exit_code;
 }
 
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  int exit_code = 0;
+  if (s21_valid(A) && s21_valid(B)) {
+    if (s21_compare_matrix(A, B)) {
+      s21_create_matrix(A->rows, A->columns, result);
+      for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < A->columns; j++) {
+          result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+        }
+      }
+    } else {
+      exit_code = 2;
+    }
+  } else {
+    exit_code = 1;
+  }
+  return exit_code;
+}
+
+int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
+  int exit_code = 0;
+  if (s21_valid(A)) {
+    s21_create_matrix(A->rows, A->columns, result);
+    for (int i = 0; i < A->rows; i++) {
+      for (int j = 0; j < A->columns; j++) {
+        result->matrix[i][j] = A->matrix[i][j] * number;
+      }
+    }
+  } else {
+    exit_code = 1;
+  }
+  return exit_code;
+}
+
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  int exit_code = 0;
+  if(s21_valid(A) && s21_valid(B)) {
+    s21_create_matrix(A->rows, B->columns, result);
+    for (int i = 0; i < A->rows; i++) {
+      for (int j = 0; j < B->columns; j++) {
+        double mul = 0;
+        for (int k = 0; k < A->columns; k++) {
+          mul += A->matrix[i][k] * B->matrix[k][j];
+        }
+        result->matrix[i][j] = mul;
+      }
+    }
+  } else {
+    exit_code = 1;
+  }
+  return exit_code;
+}
+
 void s21_print_matrix(matrix_t M) {
   for (int i = 0; i < M.rows; i++) {
     for (int j = 0; j < M.columns; j++) {
-      printf("%f ", M.matrix[i][j]);
+      printf("%.7f ", M.matrix[i][j]);
     }
     printf("\n");
   }
+  printf("\n");
 }
 
 int s21_valid(matrix_t *M) {
